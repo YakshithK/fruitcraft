b44d08a88f98517290aa62e8de49b56f797f1e03
Yakshith <yakshith.kommineni@gmail.com>
2026-06-10 23:29:49 -0400

    mango trees work now

--- src/client/java/yakshith/fruitcraft/datagen/FruitcraftDataGenerator.java
+++ src/client/java/yakshith/fruitcraft/datagen/FruitcraftDataGenerator.java
@@ -2,6 +2,8 @@ package yakshith.fruitcraft.datagen;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricDynamicRegistryProvider;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
 import yakshith.fruitcraft.world.gen.FruitcraftConfiguredFeatures;
@@ -13,6 +15,18 @@ public class FruitcraftDataGenerator implements DataGeneratorEntrypoint {
         FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
         pack.addProvider(FruitcraftRecipeProvider::new);
         pack.addProvider(FruitcraftAdvancementProvider::new);
+        pack.addProvider((output, lookup) -> new FabricDynamicRegistryProvider(output, lookup) {
+            @Override
+            protected void configure(HolderLookup.Provider registries, Entries entries) {
+                entries.addAll(registries.lookupOrThrow(Registries.CONFIGURED_FEATURE));
+                entries.addAll(registries.lookupOrThrow(Registries.PLACED_FEATURE));
+            }
+
+            @Override
+            public String getName() {
+                return "Fruitcraft World Generation";
+            }
+        });
     }
 
     @Override
